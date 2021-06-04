5a0b51c02dab2a2d65bb116670c6db663a2a3e50
Tomasz Oszkiel <tomaszoszkiel09@gmail.com>
2021-06-04 14:20:57 +0200

    Routing init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
-function App() {
-  return <h1>TaskManagerApp</h1>;
-}
+import HomePage from "./home/pages/HomePage";
+import Footer from "./shared/components/Navigation/Footer";
+import Header from "./shared/components/Navigation/Header";
+import DashboardPage from "./dashboard/pages/DashboardPage";
+
+const App = () => {
+  const [isLogged, setIsLogged] = useState(true);
+  let routes;
+
+  if (isLogged) {
+    routes = (
+      <Switch>
+        <Route path='/dashboard' exact>
+          <DashboardPage />
+        </Route>
+        <Redirect to='/dashboard' />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    );
+  }
+
+  return (
+    <Router>
+      <Header />
+      <main>
+        {isLogged ? (
+          <>
+            <aside>aside</aside>
+            <section>{routes}</section>
+          </>
+        ) : (
+          <section>{routes}</section>
+        )}
+      </main>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
